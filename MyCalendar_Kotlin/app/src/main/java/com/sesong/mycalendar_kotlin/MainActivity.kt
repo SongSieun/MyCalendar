5275ede1195e92f1a514964e9e8f009bfeb6784d
SieunSong <00sieun0608@naver.com>
2018-11-06 23:44:48 +0900

    used material design

--- MyCalendar_Kotlin/app/src/main/java/com/sesong/mycalendar_kotlin/MainActivity.kt
+++ MyCalendar_Kotlin/app/src/main/java/com/sesong/mycalendar_kotlin/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.sesong.mycalendar_kotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.sesong.mycalendar_kotlin.Todo.TodoActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(bar)
+
+        fab.setOnClickListener {
+            val addIntent = Intent(this, TodoActivity::class.java)
+            startActivity(addIntent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.app_bar_search -> toast("Fav menu item is clicked!")
+        }
+        return true
     }
 }
---
+++ MyCalendar_Kotlin/app/src/main/java/com/sesong/mycalendar_kotlin/Todo/TodoActivity.kt
@@ -0,0 +1,7 @@
+package com.sesong.mycalendar_kotlin.Todo
+
+import android.support.v7.app.AppCompatActivity
+
+class TodoActivity : AppCompatActivity() {
+
+}
